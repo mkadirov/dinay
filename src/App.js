fe8b500d3020e24cac1b39e1e78a6cdbd77ccefe
Kadirov1993 <mkodirjonov@mail.ru>
2024-02-12 17:51:17 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {Button, Box, TextField, Grid, Checkbox} from '@mui/material';
+import { locations } from './data';
+import logo from './assets/Box.png'
+//const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 function App() {
   
   const [startLocation, setStartLocation] = useState('');
   const [endLocation, setEndLocation] = useState('');
+  const [list, setList] = useState([])
+  const [refNum, setRefNum] = useState('')
+  const [marketId, setMarketId] = useState(0);
+  const [index, setIndex] = useState(0)
+  
+
+  // const handleCheckboxChange = () => {
+  //   setIndex(prev => prev + 1);
+  //   if(index <= list.length) {
+  //     setMarketId(list[index].shopId)
+  //   }
+  // };
+
+
+
+  const createList = () => {
+    const newList = locations
+    setList(newList.filter(item => item.refNumber == refNum))
+  }
+
 
   const getCurrentLocation = () => {
     // Check if Geolocation is supported
@@ -25,7 +49,9 @@ function App() {
     }
   };
 
-  const handleNavigation = () => {
+  const handleNavigation = (loc) => {
+    getCurrentLocation();
+    setEndLocation(loc)
     // Construct the Google Maps URL with the start and end locations
     const mapsUrl = `https://www.google.com/maps/dir/${startLocation}/${endLocation}`;
 
@@ -34,28 +60,42 @@ function App() {
   };
 
   return (
-    <div>
-      <label>
-        Start Location:
-        <input
-          type="text"
-          value={startLocation}
-          onChange={(e) => setStartLocation(e.target.value)}
-        />
-        <button onClick={getCurrentLocation}>Use Current Location</button>
-      </label>
-      <br />
-      <label>
-        End Location:
-        <input
-          type="text"
-          value={endLocation}
-          onChange={(e) => setEndLocation(e.target.value)}
-        />
-      </label>
-      <br />
-      <button onClick={handleNavigation}>Navigate</button>
-    </div>
+    <Box sx={{width: '100vw',  marginTop: 2}}>
+      <Box display={'flex'} >
+        <TextField 
+          id="outlined-basic" 
+          label="Sendungsnummer" 
+          variant="outlined" 
+          value={refNum}
+          onChange={(e) => setRefNum(e.target.value)}
+          />
+        <Button variant='contained' onClick={createList}>
+          Enter
+        </Button>
+        
+      </Box>
+
+      {/* <Button variant='contained' onClick={handleNavigation}>
+          go
+      </Button> */}
+      <Grid container spacing={2}>
+      {list.map((item, index) => (
+        <Grid item xs={2} key={index} sx={{marginTop: 2}}>
+          <Box style={{ order: index }}>
+            {/* <Box className="image-box">
+              <img src={logo}  />
+            </Box> */}
+
+            <Button variant='outlined' onClick={ ()=> handleNavigation(item.location)}>
+              {item.shopId}
+            </Button>
+            
+          </Box>
+        </Grid>
+      ))}
+    </Grid>
+      
+    </Box>
   );
 }
 
---
+++ src/data/index.js
@@ -0,0 +1,50 @@
+export const locations = [
+    {
+        name: 'Darvoza Savdo MCHJ',
+        adresse: 'Toshkent shahar, Muqumiy köchasi, 36-uy',
+        location: '51.468971, 7.212515',
+        refNumber: 10,
+        completed: false,
+        shopId: 1
+    },
+    {
+        name: 'Karzinka.uz Abay',
+        adresse: 'Toshkent shahar, Qoratosh-12, Zarrin köchasi, 33-uy',
+        location: '51.486718, 7.191916',
+        refNumber: 10,
+        completed: false,
+        shopId: 2
+    },
+    {
+        name: 'Makro Qoratosh',
+        adresse: 'Toshkent shahar, Mirzo Ulugbek tumani, Sarbon köchasi, 26-uy',
+        location: '51.479021, 7.250624',
+        refNumber: 12,
+        completed: false,
+        shopId: 3
+    },
+    {
+        name: 'Karzinka Malika',
+        adresse: 'Toshkent shahar, Yashnabod tumani, Bobur köchasi, 3-uy',
+        location: '51.474222, 7.209510',
+        refNumber: 10,
+        completed: false,
+        shopId: 4
+    },
+    {
+        name: 'Oqtepa Lavash',
+        adresse: 'Toshkent shahar, Yunusobod tumani, Odilov köchasi 12-uy',
+        location: '51.470676, 7.238032',
+        refNumber: 10,
+        completed: false,
+        shopId: 5
+    },
+    {
+        name: 'Evos, Chorsu',
+        adresse: 'Toshkent shahar, Mirobod tumani, Gözal köchasi, 6-uy',
+        location: '51.483374, 7.208639',
+        refNumber: 12,
+        completed: false,
+        shopId: 6
+    },
+]
